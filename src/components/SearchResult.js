68090f395bc22972e818ee84fad5563bd35c37e3
Oliver Tuck <olivertuckwebdev@gmail.com>
2019-05-05 23:25:50 +0100

    Converted SearchResult component into a class based component and added a ref to the component to access in the DOM

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FaHeart, FaDownload } from 'react-icons/fa';
 import './SearchResult.scss';
 
-const SearchResult = props => {
-  return (
-    <div className="search-result">
-      <img src={props.src} alt={props.alt} className="img-fluid" />
-      <div className="information d-flex justify-content-between px-2 px-sm-0 mt-2">
-        <div className="d-flex align-items-center">
-          <FaHeart className="mr-1 text-likes" />
-          <span className="font-weight-bold">{props.likes}</span>
+class SearchResult extends Component {
+  constructor(props) {
+    super(props);
+
+    this.resultRef = React.createRef();
+  }
+
+  render() {
+    return (
+      <div className="search-result">
+        <img src={this.props.src} alt={this.props.alt} className="img-fluid" />
+        <div className="information d-flex justify-content-between px-2 px-sm-0 mt-2">
+          <div className="d-flex align-items-center">
+            <FaHeart className="mr-1 text-likes" />
+            <span className="font-weight-bold">{this.props.likes}</span>
+          </div>
+          <a
+            href={this.props.srcFull}
+            className="d-flex align-items-center btn btn-sm btn-dark"
+            download
+          >
+            Download
+            <FaDownload className="ml-1" />
+          </a>
         </div>
-        <a
-          href={props.srcFull}
-          className="d-flex align-items-center btn btn-sm btn-dark"
-          download
-        >
-          Download
-          <FaDownload className="ml-1" />
-        </a>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default SearchResult;
