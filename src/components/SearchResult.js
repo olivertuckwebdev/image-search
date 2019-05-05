1030d2ff7cd2bb76504b543f0e9192e3a93a98b6
Oliver Tuck <olivertuckwebdev@gmail.com>
2019-05-06 00:40:02 +0100

    Added masonry styling

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -6,13 +6,38 @@ class SearchResult extends Component {
   constructor(props) {
     super(props);
 
-    this.resultRef = React.createRef();
+    this.state = { spans: 0 };
+
+    this.resultImageRef = React.createRef();
   }
+
+  componentDidMount() {
+    this.resultImageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.resultImageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+
+    console.log('called');
+
+    this.setState({
+      spans
+    });
+  };
 
   render() {
     return (
-      <div ref={this.resultRef} className="search-result">
-        <img src={this.props.src} alt={this.props.alt} className="img-fluid" />
+      <div
+        style={{ gridRowEnd: `span ${this.state.spans}` }}
+        className="search-result"
+      >
+        <img
+          ref={this.resultImageRef}
+          src={this.props.src}
+          alt={this.props.alt}
+          className="img-fluid"
+        />
         <div className="information d-flex justify-content-between px-2 px-sm-0 mt-2">
           <div className="d-flex align-items-center">
             <FaHeart className="mr-1 text-likes" />
